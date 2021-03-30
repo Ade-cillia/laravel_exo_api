d602a53fa1d4d5b4e734c606b1c0a679b49fa25f
Aurélien <aureliendu3809@outlook.fr>
2021-03-30 13:16:48 +0200

    origin

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\AuthorCollection;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new AuthorCollection(Author::all());
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newAuthor = Author::addAuthor($request->all());
+
+        return response()->json($newAuthor,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        return new AuthorResource($author);
+        
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        $updateAuthor = Author::updateAuthor($author, $request->all());
+
+        return response()->json($updateAuthor,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return response()->json('',204);
+    }
+}
